9d7f536fe7b6b9c5ed8a6d97247fbec69b9670cc
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 18:57:55 +0530

    added analysis tools

--- analytics.py
+++ analytics.py
@@ -1,5 +1,12 @@
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv("data/expenses_tracker.csv")
-print(df)
+def show_analysis():
+    df = pd.read_csv("data/expenses_tracker.csv")
+    total_spending = df['amount'].sum()
+    avg_spending = df['amount'].mean()
+    high_expense = df['amount'].max()
+    grp = df.groupby('category')['amount'].sum()
+    print(f"Total spending done is: {total_spending}")
+    print(f"Average spending done is: {avg_spending}")
+    print(f"Highest spending done is: {high_expense}")
+    print(f"Data grouped by category: \n{grp}")
