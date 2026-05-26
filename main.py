8e61171df5315d7e797e48f525bd4894d21729bc
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 18:03:47 +0530

    added option 2

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from database import create_table
 create_table()
 
 
+print("📈💰📊Welcome to Expense Tracker📈💰📊")
+name = input("Enter your name: ").capitalize()
+
 while True:
-    print("📈💰📊Welcome to Expense Tracker📈💰📊")
-    name = input("Enter your name: ").capitalize()
     obj = Expense(name)
     print("Press\n1. Add expense\n2. View expense\n3. Export CSV\n4. Show analytics\n5. Generate charts\n6. Exit")
     try:
@@ -17,8 +18,17 @@ while True:
             amount = int(input("Enter how much money spent: "))
             date = input("Enter date(2026-01-01 format): ")
             payment_method = input("Enter payment method(Cash, Card, UPI): ").capitalize()
-            note = input("Enter where money spent: ")
+            note = input("Enter where money spent: ").capitalize()
             Expense.add_expenses(category, amount, date, payment_method, note)
+            time.sleep(1)
+        elif opt == 2:
+            Expense.view_expenses()
+            time.sleep(1)
+
+        
+
+        elif opt == 6:
+            break 
 
     except ValueError:
         print("Enter only displayed options")
