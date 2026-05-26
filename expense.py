aef222bd6c3a19f20c2175f5f4c3dffd1252a2f1
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 17:35:36 +0530

    added add_expenses method

--- expense.py
+++ expense.py
@@ -1,2 +1,36 @@
+from database import get_connection
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_name = os.getenv("DATABASE_NAME")
+
+
 class Expense:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+        print(f"Hello, {self.name}")
+
+    @staticmethod
+    def add_expenses(category,
+                    amount, 
+                    date, 
+                    payment_method, 
+                    note=None):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO expenses
+            (category, amount, date, payment_method, note)
+            VALUES (?, ?, ?, ?, ?)
+            """, (
+                category,
+                amount,
+                date,
+                payment_method,
+                note
+        ))
+        conn.commit()
+        conn.close()
