a978be83dd6971ab11c00e1bbccc1720aa11d99a
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 18:44:12 +0530

    changed the file location

--- file_handler.py
+++ file_handler.py
@@ -5,7 +5,7 @@ conn = get_connection()
 
 def export_csv():
     df = pd.read_sql_query("SELECT * FROM expenses", conn)
-    df.to_csv("expenses_tracker.csv", index=False)
+    df.to_csv("data/expenses_tracker.csv", index=False)
     conn.commit()
     conn.close()
 
