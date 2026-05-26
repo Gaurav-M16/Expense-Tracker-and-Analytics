bf83b87ed833e938d91211f563cc17535b529628
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 18:28:50 +0530

    updated file handler

--- file_handler.py
+++ file_handler.py
@@ -3,6 +3,9 @@ from database import get_connection
 
 conn = get_connection()
 
-df = pd.read_sql_query("SELECT * FROM expenses", conn)
-df.to_csv("expenses_tracker.csv", index=False)
+def export_csv():
+    df = pd.read_sql_query("SELECT * FROM expenses", conn)
+    df.to_csv("expenses_tracker.csv", index=False)
+    conn.commit()
+    conn.close()
 
