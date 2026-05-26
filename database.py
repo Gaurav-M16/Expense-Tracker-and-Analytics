c4756e485534e3b5af882a60bcaf147ce0bfdfc5
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 15:26:38 +0530

    created database

--- database.py
+++ database.py
@@ -3,6 +3,16 @@ import sqlite3
 conn = sqlite3.connect("data/expenses.db")
 
 cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS expenses(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+               category TEXT,
+               amount REAL,
+               data TEXT,
+               payment_method TEXT,
+               note TEXT)
+
+""")
 
 conn.commit()
 conn.close()
