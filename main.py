c47e63e580c55ef3f764838ed0ba178be16c6135
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-26 17:36:07 +0530

    added menu with option 1

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import time
+from expense import Expense
+from database import create_table
+
+create_table()
+
 
 while True:
     print("📈💰📊Welcome to Expense Tracker📈💰📊")
+    name = input("Enter your name: ").capitalize()
+    obj = Expense(name)
     print("Press\n1. Add expense\n2. View expense\n3. Export CSV\n4. Show analytics\n5. Generate charts\n6. Exit")
     try:
         opt = int(input("Your option: "))
-        if opt == 6:
-            break
+        if opt == 1:
+            category = input("Enter category(Food, Travel, Shopping): ").capitalize()
+            amount = int(input("Enter how much money spent: "))
+            date = input("Enter date(2026-01-01 format): ")
+            payment_method = input("Enter payment method(Cash, Card, UPI): ").capitalize()
+            note = input("Enter where money spent: ")
+            Expense.add_expenses(category, amount, date, payment_method, note)
+
     except ValueError:
         print("Enter only displayed options")
         time.sleep(1)
