b5b0b38139fc8946e4dca43b86c0f91a2b93cee4
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-27 21:44:51 +0530

    defined the function

--- visualization.py
+++ visualization.py
@@ -2,14 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('data/expenses_tracker.csv')
-print(data)
-plt.figure(figsize=(9,5))
-plt.subplot(1,2,1)
-plt.title("Category wise expenses")
-sns.barplot(x=data['category'], y=data['amount'])
+def visualization():
+    data = pd.read_csv('data/expenses_tracker.csv')
+    print(data)
+    plt.figure(figsize=(9,5))
+    plt.subplot(1,2,1)
+    plt.title("Category wise expenses")
+    sns.barplot(x=data['category'], y=data['amount'])
 
-plt.subplot(1,2,2)
-plt.title("Category where most expenses are spent")
-sns.countplot(data=data, x=data['category'])
-plt.show()
+    plt.subplot(1,2,2)
+    plt.title("Category where most expenses are spent")
+    sns.countplot(data=data, x=data['category'])
+    plt.show()
