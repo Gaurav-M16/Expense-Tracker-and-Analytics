aa8e7a9fe706f30e2352f93e2aad6ac4a5104ebe
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-27 23:43:01 +0530

    added comment animation

--- animation.py
+++ animation.py
@@ -2,6 +2,9 @@ import time
 import random 
 
 def animation(word):
+    '''
+    function that do basic animation of loading 
+    '''
     print(f"{word}",end='')
     num = random.randint(3,10)
     for _ in range(1,num+1):
