3dafdcdfe6a2ddb38fb35a6ba743ab3a36bb29bb
Gaurav Methe <gaurav.methe16@gmail.com>
2026-05-27 21:11:45 +0530

    added animation.py file

---
+++ animation.py
@@ -0,0 +1,11 @@
+import time
+import random 
+
+def animation(word):
+    print(f"{word}",end='')
+    num = random.randint(3,10)
+    for _ in range(1,num+1):
+        print('.',end='',flush=True)
+        time.sleep(1)
+
+animation("Analyzing image")
